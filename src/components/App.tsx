686ac1010b88f6b34d0c5e9cef93fead0fc71894
zaully <zaully@gmail.com>
2025-11-05 21:55:44 -0500

    v0.8.3: chord styles, dotted and chord lengths

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,18 @@
-import { HashRouter, Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GitHubApiProvider } from "../context/GitHubApiProvider";
+import { GlobalUserPrefsProvider } from "../context/GlobalUserPrefProvider";
 import { ReposProvider } from "../context/ReposProvider";
 import { SnackbarProvider } from "../context/SnackbarProvider";
 import { SongPrefsProvider } from "../context/SongPrefsProvider";
 import { ThemeProvider } from "../context/ThemeProvider";
 import { YouTubeIdProvider } from "../context/YouTubeIdProvider";
-import { REPO_REGEX, REPO_REGEX_ONLY_ACCOUNT } from "../lib/constants";
+import { REPO_REGEX } from "../lib/constants";
 import AppBar from "./AppBar";
 import BranchViewer from "./BranchView";
 import FileViewer from "./FileView";
 import Edit from "./MusicMd/Edit";
 import View from "./MusicMd/View";
 import RepoViewer from "./RepoView";
-import { GlobalUserPrefsProvider } from "../context/GlobalUserPrefProvider";
 
 const App = () => (
   <YouTubeIdProvider>
@@ -33,27 +33,19 @@ const App = () => (
 );
 
 const HomeRouter = () => (
-  <BrowserRouter>
+  <Router key="home-router">
+    <Route component={AppBar} />
     <Switch>
-      <Route exact path="/app">
-        <HashRouter>
-          <Route component={AppBar} />
-          <Switch>
-            <Route path={`${REPO_REGEX}/viewer/:branch/:path+`} component={View} />
-            <Route
-              path={`${REPO_REGEX}/browser/:branch/:path*`}
-              component={FileViewer}
-            />
-            <Route path={`${REPO_REGEX}/editor/:branch/:path*`} component={Edit} />
-            <Route path={REPO_REGEX} component={BranchViewer} />
-            {/* <Route path={REPO_REGEX_ONLY_ACCOUNT} component={BranchViewer} /> */}
-            <Route exact path="/" component={RepoViewer} />
-          </Switch>
-        </HashRouter>
-      </Route>
-      <Route render={() => <Redirect to="/app/" />} />
+      <Route path={`${REPO_REGEX}/viewer/:branch/:path+`} component={View} />
+      <Route
+        path={`${REPO_REGEX}/browser/:branch/:path*`}
+        component={FileViewer}
+      />
+      <Route path={`${REPO_REGEX}/editor/:branch/:path*`} component={Edit} />
+      <Route path={REPO_REGEX} component={BranchViewer} />
+      <Route path="/" component={RepoViewer} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
